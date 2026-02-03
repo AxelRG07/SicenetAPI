041b5c156c66035a1f9b5f8fafe417a38df8f3ed
Nayelis3 <s22120217@alumnos.itsur.edu.mx>
2026-02-02 20:52:59 -0600

    Repositorio con la construcción de XMLs de login y perfil para armar las peticiones HTTP con cuerpos XML.

--- app/src/main/java/com/example/sicenetapi/data/SicenetRepository.kt
+++ app/src/main/java/com/example/sicenetapi/data/SicenetRepository.kt
@@ -1,4 +1,55 @@
 package com.example.sicenetapi.data
 
-interface SicenetRepository {
+import com.example.sicenetapi.network.RetrofitClient
+
+class SicenetRepository {
+    private val api = RetrofitClient.api
+
+    suspend fun login(matricula: String, contrasenia: String): Result<String> {
+        val soapXml = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+              <soap:Body>
+                <accesoLogin xmlns="http://tempuri.org/">
+                  <strMatricula>$matricula</strMatricula>
+                  <strContrasenia>$contrasenia</strContrasenia>
+                  <tipoUsuario>ALUMNO</tipoUsuario>
+                </accesoLogin>
+              </soap:Body>
+            </soap:Envelope>
+        """.trimIndent()
+
+        return try {
+            val response = api.login(soapXml)
+            if (response.isSuccessful && response.body() != null) {
+                Result.success(response.body()!!)
+            } else {
+                Result.failure(Exception("Error Login: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getProfile(): Result<String> {
+        val soapXml = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+              <soap:Body>
+                <getAlumnoAcademicoWithLineamiento xmlns="http://tempuri.org/" />
+              </soap:Body>
+            </soap:Envelope>
+        """.trimIndent()
+
+        return try {
+            val response = api.getProfile(soapXml)
+            if (response.isSuccessful) {
+                Result.success(response.body()!!)
+            } else {
+                Result.failure(Exception("Error Perfil: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
